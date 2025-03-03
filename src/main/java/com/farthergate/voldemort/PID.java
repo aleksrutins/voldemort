8c6820d88ae3a4f6248919cf68022d0220715003
Aleks Rutins <aleks@rutins.com>
2025-03-03 07:13:31 -0500

    Basic algorithm

--- src/main/java/com/farthergate/voldemort/PID.java
+++ src/main/java/com/farthergate/voldemort/PID.java
@@ -8,14 +8,17 @@ public class PID {
 
     public float limMaxInt;
     public float limMinInt;
+    public float limMax;
+    public float limMin;
 
     public float integrator = 0f;
     public float prevError = 0f;
     public float differentiator = 0f;
     public float prevMeasurement = 0f;
-    public float out = 0;
+    public float out = 0f;
+    public float tau = 0f;
 
-    public void update(float setpoint, float measurement) {
+    public float update(float setpoint, float measurement) {
         float error = setpoint - measurement;
 
         float proportional = kp * error;
@@ -24,5 +27,17 @@ public class PID {
 
         if(integrator > limMaxInt) integrator = limMaxInt;
         else if(integrator < limMinInt) integrator = limMinInt;
+        
+        differentiator = -(2f * kd * (measurement - prevMeasurement)
+                       + (2f * tau - t) * differentiator)
+                       / (2f * tau + t);
+
+        out = proportional + integrator + differentiator;
+        if(out > limMax) out = limMax;
+        else if(out < limMin) out = limMin;
+
+        prevError = error;
+        prevMeasurement = measurement;
+        return out;
     }
 }
